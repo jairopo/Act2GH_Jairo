da153838d4cc2b089c089c556f68dc9ad344efca
Jairo Andrades <jairo.po8@gmail.com>
2022-11-14 14:12:14 +0100

    Array de longitud 30 (Java)

--- Proyecto/src/Array20Aleatorio.java
+++ Proyecto/src/Array20Aleatorio.java
@@ -2,7 +2,7 @@ public class Array20Aleatorio {
 
   public static void main(String[] args) {
     
-    final int LONGITUDARRAY = 20;
+    final int LONGITUDARRAY = 30;
     final int RANGOINICIO = 1;
     final int RANGOFIN = 10;
     int[] numerosAleatorios = new int[LONGITUDARRAY];
